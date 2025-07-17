67784d77bd40278c02119ad8a6bd71286b182044
Pham Tang Duc Manh <ptdmanh5670@gmail.com>
2025-07-17 16:07:25 +0700

    Them nhap ten va chao nguoi dung

--- hello.cpp
+++ hello.cpp
@@ -1,6 +1,9 @@
 #include<iostream>
 using namespace std;
 int main(){
-    cout << "Hello World!" << endl;
+    string name; 
+    cout << "Enter your name: "; cin >> name; 
+    cout << "Hello, " << name << ", hihi" << endl; 
     return 0;
-}
+
+}   
